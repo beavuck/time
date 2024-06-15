d838bc49ffed3104e6796f65e4671bd72474c3d0
Alexis Drai <alexis@alexisdrai.com>
2024-06-15 16:54:03 +0200

    🙈 Ignore local stuff

--- sonar_scan.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-sonar-scanner \
-  -Dsonar.projectKey=Beavuck-Time \
-  -Dsonar.sources=. \
-  -Dsonar.host.url=http://localhost:9000 \
-  -Dsonar.token=sqp_92546c7cedcf11808a71d45ffb1731402e363a3e
-
-# Local SonarQube server, so the token doesn't matter
