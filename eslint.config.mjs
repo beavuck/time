0adeab00159fea6e6b17a6385d2f3db8f4639b15
Alexis Drai <drai.alexis@gmail.com>
2026-02-14 11:51:14 +0100

    ⚙️ Upgrade dependencies automatically

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -6,23 +6,28 @@ import tseslint from 'typescript-eslint'
 
 /** @type {import('eslint').Linter.Config[]} */
 export default [
-    {files: ['**/*.{js,mjs,cjs,ts}']},
-    {languageOptions: {globals: globals.browser}},
-    pluginJs.configs.recommended,
-    ...tseslint.configs.recommended,
-    {
-        rules: {
-            '@typescript-eslint/no-explicit-any': 'off',
-            'no-restricted-syntax': [
-                'error', {
-                    selector: 'CallExpression[callee.object.name="console"]',
-                    message: 'Don\'t use console -- use the configured logger instead',
-                },
-                'error', {
-                    selector: ':not(BinaryExpression:matches([operator="!=="], [operator="==="])) > Literal[value="null"]',
-                    message: 'Usage of "null" is deprecated except when received from legacy APIs. Use "undefined" instead',
-                },
-            ],
+  {files: ['**/*.{js,mjs,cjs,ts}']},
+  {languageOptions: {globals: globals.browser}},
+  pluginJs.configs.recommended,
+  ...tseslint.configs.recommended,
+  {
+    rules: {
+      curly: ['error', 'all'],
+      '@typescript-eslint/no-explicit-any': 'off',
+      'no-restricted-syntax': [
+        'error',
+        {
+          selector: 'CallExpression[callee.object.name="console"]',
+          message: "Don't use console -- use the configured logger instead",
         },
+        'error',
+        {
+          selector:
+            ':not(BinaryExpression:matches([operator="!=="], [operator="==="])) > Literal[value="null"]',
+          message:
+            'Usage of "null" is deprecated except when received from legacy APIs. Use "undefined" instead',
+        },
+      ],
     },
+  },
 ]
