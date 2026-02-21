54cea238ad7af42f2738be2bb34f3b1c3c1bd442
Alexis Drai <adrai@cikaba.com>
2026-02-21 14:27:17 +0100

    ⚙️ Update dev dependencies

--- src/routes/routes.ts
+++ src/routes/routes.ts
@@ -1,5 +1,5 @@
 /* tslint:disable */
-
+/* eslint-disable */
 // WARNING: This file was auto-generated with tsoa. Please do not modify it. Re-run tsoa to re-generate this file: https://github.com/lukeautry/tsoa
 import type {TsoaRoute} from '@tsoa/runtime'
 import {fetchMiddlewares, ExpressTemplateService} from '@tsoa/runtime'
